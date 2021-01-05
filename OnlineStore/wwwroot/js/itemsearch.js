f1336183bdf7c6d4d201d75cdcfafdbe126d24f7
Resident98 <bartekkupka222@gmail.com>
2021-01-05 16:48:04 +0100

    Merge pull request #8 from bartlomiejkwiatkowski/homepage---items-and-comments
    * Added most buyed items and slideshow to homepage. Added hover effect to items.
    
    * MOD: zoom and others
    
    * Added search library
    
    * Changed search bar position, added X.PagedList nugget, adjusted size of slideshow controls to their boxes
    
    * Added search function
    
    * Changed some lines
    
    * Fixed search
    
    * Modified site.min.css
    
    * Removed border-radius from search bar

---
+++ OnlineStore/wwwroot/js/itemsearch.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    var
+        content = [
+            {
+                title: 'Horse',
+                description: 'An Animal',
+            },
+            {
+                title: 'Cow',
+                description: 'Another Animal',
+            }
+        ]
+        ;
+    $('.ui.search')
+        .search({
+            source: content
+        })
+        ;
+});
